b4775ac6b745532fce3fecb768ea103cb0fbefd8
Fomenko Vladyslav <fomenkovlad93@gmail.com>
2021-08-31 13:07:14 +0300

    add function to find bg color depending on temperature

---
+++ src/helpers/calculateBgColor.js
@@ -0,0 +1,24 @@
+const calculateBgColor = temp => {
+  let result;
+
+  switch (true) {
+    case temp < -10:
+      result = '#00ffff';
+      break;
+
+    case temp >= 10 && temp < 30:
+      result = '#fff700';
+      break;
+
+    case temp > 30:
+      result = '#ff8c00';
+      break;
+
+    default:
+      break;
+  }
+
+  return result;
+};
+
+export default calculateBgColor;
