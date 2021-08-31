fcaf54820bdf276a3abaa9346ad3f7a38063ad79
Fomenko Vladyslav <fomenkovlad93@gmail.com>
2021-08-31 19:42:36 +0300

    add function for fetch by query

--- src/services/weather-service.js
+++ src/services/weather-service.js
@@ -1,15 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
-const API_KEY = "cd3ed43991acf7fa1adb46013469f81a";
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'cd3ed43991acf7fa1adb46013469f81a';
 
 export const getLocationByCoordinate = async (latitude, longitude) => {
   try {
-    console.log(Date.now());
     const response = await axios.get(
-      `${BASE_URL}?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`
+      `${BASE_URL}?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`,
     );
-    console.log(Date.now());
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherByCity = async city => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?q=${city}&units=metric&appid=${API_KEY}`,
+    );
     return response.data;
   } catch (error) {
     console.log(error);
