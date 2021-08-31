8a187ce3908adba9cf2492a46f802d5135be0845
Fomenko Vladyslav <fomenkovlad93@gmail.com>
2021-08-31 10:12:37 +0300

    add inline styles to Alert

--- src/components/AlertWarn/AlertWarn.js
+++ src/components/AlertWarn/AlertWarn.js
@@ -1,9 +1,20 @@
-import { Alert, Image } from "react-bootstrap";
-import hint from "../../images/access-hint.png";
+import { Alert, Image } from 'react-bootstrap';
+import hint from '../../images/access-hint.png';
 
 export default function AlertWarn({ onAlertClose }) {
   return (
-    <Alert variant="warning" onClose={onAlertClose} dismissible>
+    <Alert
+      variant="danger"
+      style={{
+        position: 'absolute',
+        top: '20px',
+        right: '20px',
+        maxWidth: '350px',
+      }}
+      transition
+      onClose={onAlertClose}
+      dismissible
+    >
       <Alert.Heading>
         Your browser is block share with geoposition
       </Alert.Heading>
